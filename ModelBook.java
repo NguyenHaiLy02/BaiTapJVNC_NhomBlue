3facd16d7ac69685ce18dad27fbe47ea62257714
LeDucHoang2002 <100758953+LeDucHoang2002@users.noreply.github.com>
2022-04-27 08:47:42 +0700

    Add files via upload

---
+++ ModelBook.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author win
+ */
+public class ModelBook {
+    private int id;
+    private String title;
+    private float price;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public ModelBook(int id, String title, float price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
